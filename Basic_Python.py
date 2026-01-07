8e4bdefe6faa76ec23f67b5ebde124ac92da96a5
kishoryewale5002 <kishoryewale8080@gmail.com>
2026-01-07 11:18:58 +0530

    Add files via upload
    Simple Hello Program
    Initialize Variable
    Operators
    List
    Tuple
    Dictionary
    etc

---
+++ Basic_Python.py
@@ -0,0 +1,176 @@
+""" 
+#-------------------------simple print function---------------------------------
+print("Hello Python")
+
+#-------------------------simple type checking ---------------------------------
+name="kishor"
+age=20
+marks=90
+
+print(type(name))
+print(name)
+print(type(age))
+print(age)
+print(type(marks))
+print<(marks)
+
+#------------------------Take the Input From The User---------------------------
+
+a=str(input("Enter the Fisrt Name :"))
+b=float(input("Enter the Marks" :))
+print(a)
+print(b)
+
+#--------------------------Assignment Operators---------------------------------
+#--------------------------Addition of two number-------------------------------
+a=int(input("Enter The First No :"))
+b=int(input("Enter The Scecond No :"))
+
+res=a+b
+print("The Sum is :",res)
+
+#-------------------------Sub Of two Number-------------------------------------
+
+a=int(input("Enter The First No :"))
+b=int(input("Enter The Scecond No :"))
+
+res=a-b
+print("The Sub is :",res)
+
+#-------------------------Mul of Two number-------------------------------------
+
+a=int(input("Enter The First No :"))
+b=int(input("Enter The Scecond No :"))
+
+res=a*b
+print("The Mul is :",res)
+
+a=int(input("Enter The First No :"))
+b=int(input("Enter The Scecond No :"))
+
+#-------------------------Mod of two Number--------------------------------------
+
+res=a%b
+print("The Mod is :",res)
+
+#--------------------------------------------------------------------------------
+
+a=int(input("Enter The First No :"))
+b=int(input("Enter The Scecond No :"))
+
+res=a**b
+print("The Mul is :",res)
+
+#--------------------------------------------------------------------------------
+
+#------------------------------Relational Operators-------------------------------
+
+a=20
+b=30
+
+if(a==b):
+    print("A is Equal B")
+else:
+    print("A is Not Equal To The B" )
+
+#-----------------------------------------------------------------------------------
+ 
+a=20
+b=30
+
+if(a!=b):
+    print("A is Equal B")
+else:
+    print("A is Not Equal To The B" )
+
+#-----------------------------------------------------------------------------------
+
+a=20
+b=30
+
+if(a<b):
+    print("B Is Grater")
+else:
+    print("A Is Greter" )
+
+#-----------------------------------------------------------------------------------
+
+a=20
+b=30
+
+if(a>=b):
+    print("A is big")
+else:
+    print("b is big" )
+  
+#-----------------------------------------Assignment operator------------------------
+
+# -------------------------------=,+=,-=,*=,%=,!=,**= -------------------------------
+
+#----------------------------------Logical Operator----------------------------------
+
+a=10
+b=30
+c=50
+
+if(a>b and a>c):
+    print("A is gretarer")
+elif(b>c):
+    print("B is Grater")
+else:
+    print("C is Grtarer")
+
+#-----------------------------------------------------------------------------------
+
+a=60
+b=30
+c=50
+
+if(a>b or a>c):
+    print("A is gretarer")
+elif(b>c):
+    print("B is Grater")
+else:
+    print("C is Grtarer")
+
+#------------------------------------------------------------------------------------
+   
+a=True
+print(not(a))
+
+#-------------------------------------------------------------------------------------
+  
+#--------------------------------type Conversion--------------------------------------
+                                   #Conversion
+                                  #type Casting
+#------------------------------------------------------------------------------------
+a=70
+b=2.5
+c=int("6")
+d=float("3.55")
+
+print("Automatically Conversion",a+b)
+print("type Casting",c+a)
+print(d+b)
+
+#--------------------------------------------------------------------------------------
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
